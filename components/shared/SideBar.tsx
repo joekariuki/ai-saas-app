9e5cb2d892900ad5263a4592c9224b7e4846e761
Joe Kariuki <jknguru6@gmail.com>
2024-04-06 15:06:41 +0300

    Add mobile navigation component

--- components/shared/SideBar.tsx
+++ components/shared/SideBar.tsx
@@ -85,7 +85,7 @@ const SideBar = () => {
           </SignedIn>
           <SignedOut>
             <Button asChild className="button bg-purple-gradient bg-cover">
-              <Link href="/">Login</Link>
+              <Link href="/sign-in">Login</Link>
             </Button>
           </SignedOut>
         </nav>
