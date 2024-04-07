4c398986b0c0d9267b3828a13de8e1a3280b8873
Joe Kariuki <jknguru6@gmail.com>
2024-04-07 06:32:32 +0300

    Add error handler util

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,23 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+// Error handler
+export const handleError = (error: unknown) => {
+  if (error instanceof Error) {
+    // Native JavaScript Error
+    console.error(error.message);
+    throw new Error(`Error: ${error.message}`);
+  } else if (typeof error === "string") {
+    // String error
+    console.error(error);
+    throw new Error(`Error: ${error}`);
+  } else {
+    // Unknown error type
+    console.error(error);
+    throw new Error(`Unknwon error: ${JSON.stringify(error)}`);
+  }
+};
