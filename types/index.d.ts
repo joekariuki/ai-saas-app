24ac5812a9ffe01f4e8455dd289d85c84002bfe5
Joe Kariuki <jknguru6@gmail.com>
2024-04-07 06:38:00 +0300

    Add user actions

--- types/index.d.ts
+++ types/index.d.ts
@@ -3,3 +3,19 @@ declare type NavLink = {
   route: string;
   icon: string;
 };
+
+declare type CreateUserParams = {
+  clerkId: string;
+  email: string;
+  username: string;
+  firstName: string;
+  lastName: string;
+  photo: string;
+};
+
+declare type UpdateUserParams = {
+  firstName: string;
+  lastName: string;
+  username: string;
+  photo: string;
+};
